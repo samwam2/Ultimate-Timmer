dd2c16fa84ac8088e14e4208c868248d53780841
Sam Showalter <samwam2@gmail.com>
2016-04-21 13:08:26 -0700

    Basic commit
    First commit with pods installed.

---
+++ Podfile
@@ -0,0 +1,8 @@
+use_frameworks!
+
+pod 'Mixpanel'
+pod "LaunchKit"
+pod 'FBSDKCoreKit'
+pod 'FBSDKShareKit'
+pod 'FBSDKLoginKit'
+pod 'FBAudienceNetwork'
---
+++ Ultimate Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ultimate Timer
+//
+//  Created by ShowalterS18 on 4/21/16.
+//  Copyright © 2016 Sam Showalter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
